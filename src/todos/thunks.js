ef1d213c4d742ec11d625ff36d5de30eef19e977
Irene-Mukii <renniequin@gmail.com>
2023-03-23 17:10:12 +0300

    adds and makes use of a thunk for creating new todos on the server

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,15 +1,33 @@
-import { loadTodosFailure, loadTodosInProgress, loadTodosSuccess } from "./actions";
-// does redux thunk automatically connect our side effects to the component?
+import { createTodo ,loadTodosFailure, loadTodosInProgress, loadTodosSuccess } from "./actions";
+// does redux thunk automatically connect our side effects to the component? no, call in component
 
-export const loadTodos = () => async (dispatch , getState) =>{
+export const loadTodos = () => async dispatch  =>{
     try{
     dispatch(loadTodosInProgress());
-    const response = await fetch('http://localhost:5000/todos-delay');
+    const response = await fetch('http://localhost:5000/todos');
     const todos = await response.json();
 
     dispatch(loadTodosSuccess(todos));
     } catch (e){
         dispatch(loadTodosFailure());
+        dispatch(displayAlert(e));
+    }
+};
+
+export const addTodoRequest = text => async dispatch => {
+    try {
+        const body = JSON.stringify({ text });
+        const response = await fetch('http://localhost:5000/todos', {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            method: 'post',
+            body,
+        });
+        console.log(response)
+        const todo = await response.json();
+        dispatch(createTodo(todo));
+    } catch (e) {
         dispatch(displayAlert(e));
     }
 }
