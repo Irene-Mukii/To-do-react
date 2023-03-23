7a29793d30b064a5ab59ab872ef8fcb42b0a86a4
Irene-Mukii <renniequin@gmail.com>
2023-03-23 15:49:43 +0300

    allows use of redux-thunk in the store

--- src/store.js
+++ src/store.js
@@ -5,10 +5,11 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
-import { todos } from "./todos/reducers";
+import { todos, isLoading } from "./todos/reducers";
 
 const reducers = {
-    todos
+    todos, 
+    isLoading
 };
 
 const persistConfig = {
