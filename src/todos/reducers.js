54c3109ac32d0d5329be854758cf60c5574738ca
Irene-Mukii <renniequin@gmail.com>
2023-03-23 15:45:37 +0300

    Fetches data from  api and dispatches loadTodos progress, failure and success actions to the reducer using loadTodos thunk, which changes the state accordingly

--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -1,4 +1,24 @@
-import { CREATE_TODO, REMOVE_TODO, COMPLETE_TODO } from "./actions";
+import { 
+    CREATE_TODO,
+    REMOVE_TODO,
+    COMPLETE_TODO,
+    LOAD_TODOS_FAILURE,
+    LOAD_TODOS_IN_PROGRESS,
+    LOAD_TODOS_SUCCESS
+ } from "./actions";
+
+ export const isLoading = (state=false, action) => {
+    const {type} = action;
+    switch (type){
+        case LOAD_TODOS_IN_PROGRESS: 
+            return true;
+        case LOAD_TODOS_FAILURE:
+        case LOAD_TODOS_SUCCESS: 
+            return false;
+        default:
+            return state;
+    }
+ }
 
 export const todos = (state=[], action) => {
     const { type, payload}=action;
@@ -25,6 +45,13 @@ export const todos = (state=[], action) => {
                 return todo;
             })
         }
+        case LOAD_TODOS_SUCCESS:{
+            const { todos } = payload;
+            return todos;
+        }
+        case LOAD_TODOS_IN_PROGRESS:        
+        case LOAD_TODOS_FAILURE:
+
         default:
             return state;
     }
